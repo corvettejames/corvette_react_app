6c6c15fa539b9ee1709a04d8eb61805113da8d96
corvettejames <corvettejames@icloud.com>
2023-04-30 10:33:13 -0400

    Intitial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import princess from './princess.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={princess}  alt="logo" />
+        {/* <p>
+        Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>  */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
